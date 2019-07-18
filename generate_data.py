4208e06dd7e9b9662f566b4d03e3eb47da882995
liguiyuan <820001401@qq.com>
2019-07-18 15:11:52 +0800

    add euler_angles to enerate_data.py

--- generate_data.py
+++ generate_data.py
@@ -3,16 +3,17 @@ import numpy as np
 import cv2
 
 def DateSet(file_list, args, debug=False):
-    file_list, landmarks, attributes = gen_data(file_list)
+    file_list, landmarks, attributes, euler_angles = gen_data(file_list)
     if debug:
         n = args.batch_size * 10
         file_list = file_list[:n]
         landmarks = landmarks[:n]
         attributes = attributes[:n]
+        euler_angles = euler_angles[:n]
 
-    dataset = tf.data.Dataset.from_tensor_slices((file_list, landmarks, attributes))
+    dataset = tf.data.Dataset.from_tensor_slices((file_list, landmarks, attributes, euler_angles))
 
-    def _parse_data(filename, landmarks, attributes):
+    def _parse_data(filename, landmarks, attributes, euler_angles):
         # filename, landmarks, attributes = data
         file_contents = tf.read_file(filename)
         image = tf.image.decode_png(file_contents, channels=args.image_channels)
@@ -22,7 +23,7 @@ def DateSet(file_list, args, debug=False):
         image = tf.cast(image, tf.float32)
 
         image = image / 256.0
-        return (image, landmarks, attributes)
+        return (image, landmarks, attributes,euler_angles)
 
     dataset = dataset.map(_parse_data)
     dataset = dataset.shuffle(buffer_size=10000)
@@ -31,22 +32,27 @@ def DateSet(file_list, args, debug=False):
 def gen_data(file_list):
     with open(file_list,'r') as f:
         lines = f.readlines()
-    filenames, landmarks,attributes = [], [], []
+    filenames, landmarks,attributes,euler_angles = [], [], [], []
     for line in lines:
         line = line.strip().split()
         path = line[0]
         landmark = line[1:197]
-        attribute = line[197:]
+        attribute = line[197:203]
+        euler_angle = line[203:206]
 
         landmark = np.asarray(landmark, dtype=np.float32)
         attribute = np.asarray(attribute, dtype=np.int32)
+        euler_angle = np.asarray(euler_angle, dtype=np.float32)
         filenames.append(path)
         landmarks.append(landmark)
         attributes.append(attribute)
+        euler_angles.append(euler_angle)
+        
     filenames = np.asarray(filenames, dtype=np.str)
     landmarks = np.asarray(landmarks, dtype=np.float32)
     attributes = np.asarray(attributes, dtype=np.int32)
-    return (filenames, landmarks, attributes)
+    euler_angles = np.asarray(euler_angles, dtype=np.float32)
+    return (filenames, landmarks, attributes, euler_angles)
 
 
 if __name__ == '__main__':
